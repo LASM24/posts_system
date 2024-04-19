6ffdb9fedc61435e78cf1985091be04cdc9bfc2a
Luis Mendez <130018122+LASM24@users.noreply.github.com>
2024-04-19 17:47:47 -0500

    Post app: Detail view for individual posts. (#3)
    Detail view for individual posts implemented, serving as the foundation for future CRUD actions

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,7 @@
 from django. urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 
 posts_patterns = ([
-    path('', PostListView.as_view(), name='posts')
+    path('', PostListView.as_view(), name='posts'),
+    path("<int:pk>/<slug:slug>/", PostDetailView.as_view(), name="post"),
 ], 'posts')
