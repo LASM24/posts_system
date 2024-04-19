ab7dd5466649b13d237970dcc1c82ebae2e0f335
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-04-19 17:42:59 -0500

    Post app: Detail view for individual posts

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,13 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 
 # Create your views here.
 class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
     model = Post
