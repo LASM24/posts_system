ffcd87774f74f9c23cecbeda747a390b8b77ddb5
Luis Mendez <130018122+LASM24@users.noreply.github.com>
2024-04-21 17:45:59 -0500

    Post app: Post creation functionality added. (#6)
    Admins can now create posts directly from the template

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,8 @@
 from django. urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView
 
 posts_patterns = ([
     path('', PostListView.as_view(), name='posts'),
     path("<int:pk>/<slug:slug>/", PostDetailView.as_view(), name="post"),
+    path('create/', PostCreateView.as_view(), name='create')
 ], 'posts')
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,10 @@
 # from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
+from .forms import PostCreateForm
 
 
 # Create your views here.
@@ -11,3 +14,9 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostCreateForm
+    success_url = reverse_lazy('posts:posts')
