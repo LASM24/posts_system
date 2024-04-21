4ce2b34ce6721c6e0e9adc5c3a2eed2b11e5664d
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-04-21 17:24:01 -0500

    Post app: Post creation functionality added

--- posts/admin.py
+++ posts/admin.py
@@ -4,7 +4,7 @@ from .models import Post
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'order')
+    list_display = ('title',)
 
 
 admin.site.register(Post, PostAdmin)
