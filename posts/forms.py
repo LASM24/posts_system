95736cb9ba46e7148d34d5945a3dfbfefe9313a9
Luis Mendez <130018122+LASM24@users.noreply.github.com>
2024-04-22 20:43:39 -0500

    Home app: new style update (#9)
    * Home app: Handles user reception. Added groundwork for future versions
    
    * Post app: lists posts.
    
    * Post app: Detail view for individual posts
    
    * Post app: Admin mini-menu added
    
    * Home & Posts app: Responsive design
    
    * Post app: Post creation functionality added
    
    * Post app: Post deletion feature added
    
    * Post app: Updated styles
    
    * Home app: new style update:  Basic styles in the posts views have been updated to enhance the web appearance.

--- posts/forms.py
+++ posts/forms.py
@@ -6,3 +6,7 @@ class PostCreateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'image']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-title'}),
+            'content': forms.Textarea(attrs={'class': 'form-content'}),
+        }
