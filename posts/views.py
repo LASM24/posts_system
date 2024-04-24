b07f9658b774b34b6905d0ca92d11093d4c0bd50
LASM24 <luisalbertosanabriamendez@gmail.com>
2024-04-24 17:33:52 -0500

    Post app: post update.

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Post
-from .forms import PostCreateForm
+from .forms import PostCreateForm, PostUpdateForm
 
 
 # Create your views here.
@@ -29,3 +30,23 @@ class PostDeleteView(DeleteView):
 
     def get_success_url(self):
             return reverse_lazy('posts:posts') + '?ok' 
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    form_class = PostUpdateForm
+    template_name_suffix = "_update_form"
+
+
+    def get_success_url(self):
+            return reverse_lazy('posts:update', args=[self.object.id]) + '?or' 
+
+    def form_valid(self, form):
+            instance = form.save(commit=False)
+
+            if 'image' in self.request.FILES:
+                instance.image = self.request.FILES['image']
+
+            instance.save()
+
+            return HttpResponseRedirect(self.get_success_url())
